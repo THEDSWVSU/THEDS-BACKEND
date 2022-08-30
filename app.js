ce1a8f8b20c332260874fd8061bb074a6338afa4
EmmsDevs <emman199810@gmail.com>
2022-08-30 08:41:13 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var cors = require('cors')
+const path = require('path')
+const conn = require('./DbConnection').con
+
+
+const driverRoute = require("./routes/driverRoutes")
+const passengerRoute = require("./routes/passengerRoutes")
+
+const PORT = 4000
+
+
+
+app.use(cors({origin:'*'}))
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+app.use('/driver', driverRoute)
+app.use('/passenger', passengerRoute)
+
+app.use("/",(req, res)=>{
+    res.send("Hey You")
+})
+
+app.listen(PORT, ()=>{
+    console.log("Server is listening at port: "+PORT)
+})
+conn.connect(function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("Database connected")
+    
+  });
+module.exports = app
