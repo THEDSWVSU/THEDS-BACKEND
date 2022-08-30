2c5778a9563585e784297b737a592d7a03822934
jonalyne <jonalynegarde@gmail.com>
2022-08-30 17:52:35 +0800

    Test if post request is reaching the route

--- routes/passengerRoutes.js
+++ routes/passengerRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
+const passengerController = require("./../controllers/passengerController")
 router.post("/register",(req, res)=>{
-    
+    passengerController.registerPassenger(req, res)
 })
 
 module.exports = router
