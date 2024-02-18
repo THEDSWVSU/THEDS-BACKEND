dc9c0f6b2f494fa26cdb1e5ff8cf34e16a6124aa
EmmsDevs <emman199810@gmail.com>
2024-02-18 22:32:42 +0800

    db connection to hostinger

--- dbConnection.js
+++ dbConnection.js
@@ -1,7 +1,8 @@
 var mysql = require('mysql');
 
 exports.con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database:"theds"
+  host: "sb-navigator.online",
+  user: "u224320190_theds",
+  password:"d8~VFEiv|J",
+  database:"u224320190_theds"
 });
